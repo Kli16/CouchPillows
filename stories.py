7bca87aab94e2ec681993e92f9b8b51f5f1bd237
Kristin Lin <klin11@stuy.edu>
2017-10-29 11:47:14 -0400

    should be clearer now

--- stories.py
+++ stories.py
@@ -6,9 +6,12 @@ import sqlite3
 stories = Flask(__name__)
 stories.secret_key = 'random'
 
-#USER = "user1"
-#PASS = "pass1"
 
+#============================================================
+# LOG IN / LOG OUT ROUTES
+
+# authentication helper fxn
+#------------------------------------------------
 AUTHENTICATED = 1
 BADUSER = -1
 BADPASSWORD = -2
@@ -21,8 +24,12 @@ def authenticate(user, password):
             return BADPASSWORD
     else:
         return BADUSER
- 
 
+    
+#root: checks if user in session
+# REDIRECTS: home
+# RENDERS: login
+#-------------------------------------------------
 @stories.route('/')
 def root():
     if 'user' in session:
@@ -30,6 +37,11 @@ def root():
     else:
         return render_template('login.html')
 
+    
+#login: takes in input from form in login.html; authenticates;
+# REDIRECTS: home, root
+# RENDERS: 
+#-------------------------------------------------
 @stories.route('/login', methods=['POST', 'GET'])
 def login():
     username = request.form['usr']
@@ -47,13 +59,30 @@ def login():
         pass
     return redirect(url_for('root'))
 
-@stories.route('/signup', methods=['POST'])
+
+#signup : checks if user is not in session
+# REDIRECTS: home
+# RENDERS: sign_up
+#-------------------------------------------------
+@stories.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if 'user' not in session:
+        return render_template("sign_up.html")
+    else :
+        return redirect(url_for('home'))
+
+    
+#signing: updates db's users
+# REDIRECTS: root
+# RENDERS:
+#-------------------------------------------------
+@stories.route('/signing', methods = ['POST'])
+def signing():
     if 'user' not in session:
         username = request.form['usr']
         password = request.form['pwd']
         passwordconf = request.form['cpwd']
-
+        print(username)
 #CODE NOT WORKING YET
 #        if username #is in DATABASE:
 #            flash("Username taken!")
@@ -66,6 +95,44 @@ def signup():
 #        flash ("You are already signed in!")
 #CODE NOT WORKING YET
 
+
+#logout: pops user off session
+# REDIRECTS: root
+# RENDERS: 
+#-------------------------------------------------
+@stories.route('/logout', methods = ['POST'])
+def logout():
+    if 'user' in session:
+        session.pop('user')
+        #print "testingg"
+    return redirect(url_for('root'))
+
+
+
+#============================================================
+# HOME 
+
+#home: shows list of contributed stories
+# REDIRECTS: root
+# RENDERS: home
+#-------------------------------------------------
+@stories.route('/home', methods = ['POST', 'GET'])
+def home():
+    if 'user' in session:
+        return render_template('home.html',
+                               stories = storiesDB.getCStories(session['user']))
+    else:
+        return redirect(url_for('root'))
+
+
+    
+#============================================================
+# ACCOUNT ROUTES
+
+#profile: checks if user in session
+# REDIRECTS: root
+# RENDERS: profile
+#-------------------------------------------------
 @stories.route('/profile', methods=['POST', 'GET'])
 def profile():
     if 'user' in session:
@@ -74,6 +141,24 @@ def profile():
     else:
         return redirect(url_for('root'))
 
+
+#profiling: updates db with new user info
+# REDIRECTS: root
+# RENDERS: 
+#-------------------------------------------------
+@stories.route('/profiling', methods = ['POST'])
+def profiling():
+    return 0
+
+
+
+#============================================================    
+# VIEW CONTRIBUTED STORY ROUTE
+
+#story: shows full story (retrieved from db) + contributers (including user)
+# REDIRECTS: root
+# RENDERS: story
+#-------------------------------------------------
 @stories.route('/story', methods = ['POST'])
 def story():
     if 'user' in session:
@@ -85,20 +170,39 @@ def story():
     else:
         return redirect(url_for('root'))
 
-    '''
-@stories.route('/change', methods = ['POST'])
-def change():
-    if 'user' in session:
-       ''' 
 
-@stories.route('/home', methods = ['POST', 'GET'])
-def home():
+
+#============================================================    
+# CREATE ROUTES
+
+#create_stories: gives user form to input new story info
+# REDIRECTS: root
+# RENDERS: create
+#-------------------------------------------------
+@stories.route('/createstory', methods = ['POST', 'GET'])
+def create_stories():
     if 'user' in session:
-        return render_template('home.html',
-                               stories = storiesDB.getCStories(session['user']))
+        return render_template("create.html")
     else:
-        return redirect(url_for('root'))
+        return render_template(url_for('root'))
 
+
+#creating: puts user input into database; updates stories and contributions
+# REDIRECTS: home
+# RENDERS: 
+#-------------------------------------------------
+@stories.route('/creating', methods = ['POST'])
+def creating():
+    return 0
+    
+
+#============================================================    
+# CONTRIBUTE ROUTES
+    
+#all_stories: shows all stories the user can contribute to
+# REDIRECTS: root
+# RENDERS: story
+#-------------------------------------------------
 @stories.route('/all_stories', methods = ['POST', 'GET'])
 def all_stories():
     if 'user' in session:
@@ -106,8 +210,13 @@ def all_stories():
     else:
         return render_template(url_for('root'))
 
+
+#contribute: gives user last update and form for next update
+# REDIRECTS: root
+# RENDERS: contribute
+#-------------------------------------------------
 @stories.route('/contribute', methods = ['POST'])
-def contribute_to_stories():
+def contribute():
     if 'user' in session:
         story = request.form['link']
         return render_template("contribute.html",
@@ -116,19 +225,21 @@ def contribute_to_stories():
     else:
         return render_template(url_for('root'))
 
-@stories.route('/createstory', methods = ['POST', 'GET'])
-def create_stories():
-    if 'user' in session:
-        return render_template("create.html")
-    else:
-        return render_template(url_for('root'))
+    
+#contributing: updates db's contributions and stories with user input
+# REDIRECTS: home
+# RENDERS: 
+#-------------------------------------------------
+@stories.route('/contributing', methods = ['POST'])
+def contributing():
+    return 0
+        
 
-@stories.route('/logout', methods = ['POST'])
-def logout():
-    if 'user' in session:
-        session.pop('user')
-        #print "testingg"
-    return redirect(url_for('root'))
+
+
+
+#============================================================
+# RUNNING
 
 if __name__ == '__main__':
     stories.debug = True
